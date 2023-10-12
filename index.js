27e983329eb1e305c82112500ea574aeaa991dbc
Pablo Tertuliano <52987435+pfts@users.noreply.github.com>
2023-10-11 23:28:31 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// Variáveis
+let nomeDoHeroi = "Kratos";
+let xpDoHeroi = 6000; // Substitua pela quantidade de XP do herói
+
+// Estrutura de decisão para classificar o nível
+let nivelDoHeroi;
+
+if (xpDoHeroi < 1000) {
+  nivelDoHeroi = "Ferro";
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+  nivelDoHeroi = "Bronze";
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+  nivelDoHeroi = "Prata";
+} else if (xpDoHeroi >= 6001 && xpDoHeroi <= 7000) {
+  nivelDoHeroi = "Ouro";
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+  nivelDoHeroi = "Platina";
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+  nivelDoHeroi = "Ascendente";
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+  nivelDoHeroi = "Imortal";
+} else {
+  nivelDoHeroi = "Radiante";
+}
+
+// Exibindo a mensagem com o nome e o nível do herói
+console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivelDoHeroi}`);
